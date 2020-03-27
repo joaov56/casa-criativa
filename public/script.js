b7873d46388a2de656c68b0c9dd828baca365c46
Joao Oliveira <vitorjao56@gmail.com>
2020-03-27 00:01:06 -0300

    documentos estaticos (css, png, script)

---
+++ public/script.js
@@ -0,0 +1,13 @@
+function onOff(){
+    document    
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hide-scroll")
+
+}
+
